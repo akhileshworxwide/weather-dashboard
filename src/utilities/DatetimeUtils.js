3af51d904afa15c100c43b93b3221990c1d90d7a
akhilesh <akhilesh.s@customerxps.com>
2024-06-22 13:41:36 +0530

    assignment setup feature completion

---
+++ src/utilities/DatetimeUtils.js
@@ -0,0 +1,25 @@
+import { MONTHS } from './DateConstants';
+
+const date = new Date();
+
+export function getDayMonthFromDate() {
+  const month = MONTHS[date.getMonth()].slice(0, 3);
+  const day = date.getUTCDate();
+
+  return day + ' ' + month;
+}
+
+export function transformDateFormat() {
+  const month = date.toLocaleString('en-US', { month: '2-digit' });
+  const day = date.toLocaleString('en-US', { day: '2-digit' });
+  const year = date.getFullYear();
+  const time = date.toLocaleString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hourCycle: 'h23',
+  });
+
+  const newFormatDate = year.toString().concat('-', month, '-', day, ' ', time);
+  return newFormatDate;
+}
